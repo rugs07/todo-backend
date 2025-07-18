6ebfb7a3797a49c78d1127ab375852614423903c
rugwedb07 <rugwed.b@mizzle.io>
2025-07-18 15:34:20 +0530

    python code updated

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
@@ -45,5 +46,6 @@ def update_task(task_id):
     return jsonify({'error': 'Task not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
